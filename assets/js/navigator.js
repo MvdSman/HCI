a0ad1c3e83c7d004c5e496034238fc831063a45d
Mark vd S <30930351+MvdSman@users.noreply.github.com>
2018-11-21 21:46:44 +0100

    First implementation navigator

---
+++ assets/js/navigator.js
@@ -0,0 +1,51 @@
+$(function() {
+    smoothScroll(1000);
+});
+  
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $( $(this).attr('href') );
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, duration);
+        }
+    });
+}
+
+var $window = $(window);
+var topLink = $("a[href='#top']"),
+    abstractLink = $("a[href='#abstract']"),
+    introductionLink = $("a[href='#introduction']"),
+    methodsLink = $("a[href='#methods']"),
+    resultsLink = $("a[href='#results']"),
+    conclusionLink = $("a[href='#conclusion']"),
+    citationsLink = $("a[href='#citations']");
+
+$window.on("scroll",function(e){
+    if($window.scrollTop() < $("#abstract").offset().top) {
+        $("#nav-wrap").find("a").css("font-weight",400);
+        topLink.css("font-weight",900);
+    } else if ($window.scrollTop() >= $("#abstract").offset().top && $window.scrollTop() < $("#introduction").offset().top) {
+        $("#nav-wrap").find("a").css("font-weight",400);
+        abstractLink.css("font-weight",900);
+    } else if ($window.scrollTop() >= $("#introduction").offset().top && $window.scrollTop() < $("#methods").offset().top) {
+        $("#nav-wrap").find("a").css("font-weight",400);
+        introductionLink.css("font-weight",900);
+    } else if ($window.scrollTop() >= $("#methods").offset().top && $window.scrollTop() < $("#results").offset().top) {
+        $("#nav-wrap").find("a").css("font-weight",400);
+        methodsLink.css("font-weight",900);
+    } else if ($window.scrollTop() >= $("#results").offset().top && $window.scrollTop() < $("#conclusion").offset().top) {
+        $("#nav-wrap").find("a").css("font-weight",400);
+        resultsLink.css("font-weight",900);
+    } else if ($window.scrollTop() >= $("#conclusion").offset().top && $window.scrollTop() < $("#citations").offset().top) {
+        $("#nav-wrap").find("a").css("font-weight",400);
+        conclusionLink.css("font-weight",900);
+    } else if ($window.scrollTop() >= $("#citations").offset().top) {
+        $("#nav-wrap").find("a").css("font-weight",400);
+        citationsLink.css("font-weight",900);
+    }
+});  
